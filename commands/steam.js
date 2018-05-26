d3a9d965a9f6fae93fb40d6df21dcdf0aedd4a42
WilsonGaara <37121741+WilsonGaara@users.noreply.github.com>
2018-05-26 17:21:52 -0300

    Update steam.js

--- commands/steam.js
+++ commands/steam.js
@@ -1,6 +1,6 @@
 const SteamAPI = require('steamapi');
 const Discord = require('discord.js')
-const steam = new SteamAPI('D5D2E6CF8B0377EFB792D564FBB66C30');
+const steam = new SteamAPI(process.env.STEAM);
 const moment = require('moment');
 	   moment.locale('pt-BR');  
 exports.run = (client, message, args)  => {
@@ -8,9 +8,9 @@ exports.run = (client, message, args)  => {
         steam.getUserSummary(id).then(summary => {
     const embed = new Discord.RichEmbed()
     .setAuthor(summary.nickname)
-.setThumbnail(summary.avatar.medium)
+.setThumbnail(summary.avatar)
 .setColor('GOLD')
-.setDescription('Ultimo logoff: '+moment(summary.lastLogOff).format(`lll`))
+.setDescription(':date: | Data de criação: '+moment(summary.lastLogOff).format(`LLL`))
 .setFooter(`ID do usuário: ${summary.steamID} | kikbot-`)
 message.channel.send({embed: embed});
 });
