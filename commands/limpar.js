4c58325235e7dee752b8e2a6e9949932ccb96ee1
WilsonGaara <37121741+WilsonGaara@users.noreply.github.com>
2018-05-26 15:06:37 -0300

    Update limpar.js

--- commands/limpar.js
+++ commands/limpar.js
@@ -1,13 +1,13 @@
 exports.run = (client, message, args)  => {
 
-    if (message.content.includes('1000')) {
+    if (if(args.join(' ').length > 100)) {
         message.channel.send(`Opa, acho que mais de 100 não posso!`)
     } else {
         if(!args[0]) return message.reply(`Coloque algum número depois do comando :x:`)
             if  (!message.member.hasPermissions(["MANAGE_MESSAGES"])) return message.reply("**Você não tem a permissão para usar isto!**");
             let messagecount = parseInt
 message.channel.bulkDelete(args[0])
-.catch(error => message.reply(`Oops! Erro: ${error}`)) 
+.catch(error => message.reply(`<:err:449743511391305748> **|** Oops! Você excedeu o limite de mensagens para serem excluidas, eu acho.`)) 
 
 
     }
