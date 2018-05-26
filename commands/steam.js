7f85e1b6130f30fcdebc3e93ec8cf4c9da124199
WilsonGaara <37121741+WilsonGaara@users.noreply.github.com>
2018-05-26 17:40:49 -0300

    Update steam.js

--- commands/steam.js
+++ commands/steam.js
@@ -10,7 +10,7 @@ exports.run = (client, message, args)  => {
     .setAuthor(summary.nickname)
 .setThumbnail(summary.avatar.large)
 .setColor('GOLD')
-.setDescription(':date: | Data de criação: '+moment(summary.created).format(`LLL`)+'\nUltimo logOFF: '+moment(summary.lastLogOff).format(`LLL`))
+.setDescription('Permissão de comentário: '+summary.commentPermission)
 .setFooter(`ID do usuário: ${summary.steamID} | kikbot-`)
 message.channel.send({embed: embed});
 });
