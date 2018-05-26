774da42512c95d352677f94d55e298e67ca34696
WilsonGaara <37121741+WilsonGaara@users.noreply.github.com>
2018-05-26 15:52:44 -0300

    Update limpar.js

--- commands/limpar.js
+++ commands/limpar.js
@@ -1,4 +1,4 @@
-exports.run = (client, message, args)  => {
+    exports.run = (client, message, args)  => {
 
         const Discord2 = require('discord.js');
        
@@ -13,9 +13,11 @@ exports.run = (client, message, args)  => {
                    if  (!message.member.hasPermissions(["MANAGE_MESSAGES"])) return message.reply("**Você não tem a permissão para usar isto!**");
                    let messagecount = parseInt
        message.channel.bulkDelete(args[0])
-     if(!args) return message.channel.send(`<:err:449743511391305748> **|** Oops! Você excedeu o limite de mensagens para serem excluidas, eu acho. Outras causas:\nVocê não colocou um número.\nO número é negativo.\nE uma infinidade de erros.`);
-       
+     if(!args.join(' ') > 100) { return message.channel.send(`<:err:449743511391305748> **|** Oops! Você excedeu o limite de mensagens para serem excluidas, eu acho. Outras causas:\nVocê não colocou um número.\nO número é negativo.\nE uma infinidade de erros.`);
+        if(!args.join(' ') < 2) { message.channel.send(`<:err:449743511391305748> **|** Oops! Você excedeu o limite de mensagens para serem excluidas, eu acho. Outras causas:\nVocê não colocou um número.\nO número é negativo.\nE uma infinidade de erros.`);
+      } else {
             message.channel.send(`:rocket: **|** Foram deletadas **${args.join(' ')}** mensagens.`);
-
-      }
+      };
+    };
+};
        
