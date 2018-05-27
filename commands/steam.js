80b065fd6ee2b882945b48c7578523ac242a0904
WilsonGaara <37121741+WilsonGaara@users.noreply.github.com>
2018-05-27 17:58:07 -0300

    Update steam.js

--- commands/steam.js
+++ commands/steam.js
@@ -1,26 +1,16 @@
-const SteamAPI = require('steamapi');
-const Discord = require('discord.js')
-const steam = new SteamAPI(process.env.STEAM);
-const moment = require('moment');
-	   moment.locale('pt-BR');  
 exports.run = (client, message, args)  => {
     steam.resolve(args.join(' ')).then(id => {
         steam.getUserSummary(id).then(summary => {
     const embed = new Discord.RichEmbed()
-    .setAuthor(summary.nickname)
+    .setAuthor(summary.nickname, summary.avatar.large)
 .setThumbnail(summary.avatar.large)
 .setColor('BLUE')
-.setFooter(`ID do usuário: ${summary.steamID} | kikbot-`)
+.setFooter(`ID STEAM do usuário: ${summary.steamID} | kikbot-`)
 message.channel.send({embed: embed});
-		steam.resolve(args.join(' ')).then(id2 => {
-		steam.getUserOwnedGames(id2).then(jogo => {
-   
-      
-
-		message.reply(`https://store.steampowered.com/app/${jogo.appID}`)
+		getUserRecentGames(id).then(games => {
+      const jogos = games.appID.map(g =>+'https://store.steampowered.com/app/'+g).join("\n")
 });
 });
     });
-               });
-              };
-            
+               };
+              
