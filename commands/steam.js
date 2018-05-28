53c3859158bb3a6fe46c2e1f98a531e0119d2456
WilsonGaara <37121741+WilsonGaara@users.noreply.github.com>
2018-05-28 15:39:10 -0300

    Update steam.js

--- commands/steam.js
+++ commands/steam.js
@@ -2,8 +2,10 @@ const SteamAPI = require('steamapi');
 const Discord = require('discord.js')
 const steam = new SteamAPI(process.env.STEAM);
 const moment = require('moment');
-	   moment.locale('pt-BR');  
+     moment.locale('pt-BR');  
+
 exports.run = (client, message, args)  => {
+  if(args.join(' ').length < 1) return message.reply('<:err:449743511391305748> **|** Você precisa inserir um id da steam válido, o que vem depois de steamcommunity.com/profiles/**seuid** ele pode até ser o seu personalizado.');
     steam.resolve(args.join(' ')).then(id => {
         steam.getUserSummary(id).then(summary => {
     const embed = new Discord.RichEmbed()
@@ -12,8 +14,16 @@ exports.run = (client, message, args)  => {
 .setColor('BLUE')
 .setFooter(`ID STEAM do usuário: ${summary.steamID} | kikbot-`)
 message.channel.send({embed: embed});
+
 		steam.getUserRecentGames(id).then(games => {
       const jogos = games.map(g => g.name).join("\n")
+      function afkchannel(){
+        var afk = jogos;
+        if(afk.length < 1){
+        return afk;
+        }else{
+            return "<:err:449743511391305748> **|** Não detectei nenhum jogo recente.";
+        }
       const Discord2 = require('discord.js')
       const embed2 = new Discord2.RichEmbed()
    .setAuthor('Jogos recentes de: '+summary.nickname, games.iconURL)
@@ -22,8 +32,12 @@ message.channel.send({embed: embed});
    .setTimestamp()
    .setFooter('kikbot- | Steam', client.user.avatarURL)
   message.channel.send({embed: embed2});
-});
+        
+};
+      
 });
     });
-               };
+  });
+};
               
+
