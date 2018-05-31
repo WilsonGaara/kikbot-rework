de8a2c625270d75c6090167b71f46a7127a4fa9a
WilsonGaara <37121741+WilsonGaara@users.noreply.github.com>
2018-05-31 18:06:13 -0300

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -7,10 +7,10 @@ exports.run = (client, message, args, game)  => {
     userstatus = message.mentions.users.first().presence.status;
     usertag = message.mentions.users.first().tag;
     usercolorole = message.guild.member(message.mentions.users.first()).displayHexColor;
-    usercriado = message.mentions.users.first().createdTimestamp
+    usercriado = message.mentions.users.first().createdTimestamp;
     userid = message.mentions.users.first().id;
-  game2 = message.mentions.users.first().presence.game
-  userentrou = message.guild.member(message.mentions.users.first().id).joinedTimestamp
+  game2 = message.mentions.users.first().presence.game;
+  userentrou = message.guild.member(message.mentions.users.first().id).joinedTimestamp;
   usercargo = message.guild.member(message.mentions.users.first().id).roles.size;
   userbot = message.mentions.users.first().bot;
   
@@ -20,10 +20,10 @@ exports.run = (client, message, args, game)  => {
       userstatus = message.author.presence.status;
     usertag = message.author.tag;
     usercolorole = message.author.highestRole.displayHexColor;
-    usercriado = message.author.createdTimestamp
+    usercriado = message.author.createdTimestamp;
     userid = message.author.id;
-    game2 = message.author.presence.game
-    userentrou = message.guild.member(message.author.id).joinedTimestamp
+    game2 = message.author.presence.game;
+    userentrou = message.guild.member(message.author.id).joinedTimestamp;
     usercargo = message.guild.member(message.author.id).roles.size;
     userapelido = message.guild.member(message.author.id).nickname
     userbot = message.author.bot;
