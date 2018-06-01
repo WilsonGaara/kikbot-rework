a6bb2598b90aa41b44320235298e8d3cd3bd01b4
WilsonGaara <37121741+WilsonGaara@users.noreply.github.com>
2018-06-01 15:19:34 -0300

    Update prefix.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -4,8 +4,7 @@ const Discord = require('discord.js')
 
         if  (!message.member.hasPermissions(["MANAGE_GUILD"])) return message.reply("Você não tem direito de mudar meu prefix!");
         if(args.join(' ').length < 1) return message.reply('Uso correto: k!prefix <prefixo>')
-        if(args.join(' ').length > 4) return message.reply('O prefix não pode ser muito longo para usuários lembrar!')
-
+      
         let prefixes = JSON.parse(fs.readFileSync("./prefixos.json", "utf8"));
         prefixes[message.guild.id] = {
             prefixes: args[0]
