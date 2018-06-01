961e3a7d918e2c5d20ffddd326174f43a68da69e
WilsonGaara <37121741+WilsonGaara@users.noreply.github.com>
2018-06-01 15:47:50 -0300

    Update prefix.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,25 +1,19 @@
-    exports.run = async(client, message, args) => {
+   exports.run = async(client, message, args) => {
 const fs = require('fs')
 const Discord = require('discord.js')
 
         if  (!message.member.hasPermissions(["MANAGE_GUILD"])) return message.reply("Você não tem direito de mudar meu prefix!");
         if(args.join(' ').length < 1) return message.reply('Uso correto: k!prefix <prefixo>')
         
-        let prefixes = JSON.parse(fs.readFileSync("./prefixos.json", "utf8"));
-        prefixes[message.guild.id] = {
-            prefixes: args[0]
-        };
-        fs.writeFile("./prefixos.json"), JSON.stringify(prefixes), (err) => {
+   let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
 
+   prefixes[message.guild.id] = {
+       prefixes: args[0]
         };
+        fs.writeFile("./prefixes.json", JSON.stringify(prefixes), (err) => {
+            if (err) console.log(err)
+        });
         message.channel.send('🚀 **|** O prefixo foi mudado para **'+args[0]+'** com êxito.')
-
-
-
-
-
-
-
 
 
     }
