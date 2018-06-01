3864ef9130ceca9feac512545ef853ba2fe6dda4
WilsonGaara <37121741+WilsonGaara@users.noreply.github.com>
2018-06-01 15:28:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
   const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
+const config = require('./config.json')
 
 
 
@@ -66,18 +67,18 @@ client.on(eventName, (...args) => eventFunction.run(client, ...args));
 });
 
 client.on("message", message => {
-let prefixes = JSON.parse(fs.readFileSync("./prefixos.json", "utf8"));
-    if(!prefixes[message.guild.id]){
-        prefixes[message.guild.id] = {
-            prefixes: "k!"
-        };
-    }
-    let prefix = prefixes[message.guild.id].prefixes;
+
   if(!message.guild) return;
   if (message.author.bot) return;
   if (!message.content.startsWith(prefix)) return;
  //Anti-Comando errado
-
+ let prefixes = JSON.parse(fs.readFileSync("./prefixos.json", "utf8"));
+ if(!prefixes[message.guild.id]){
+     prefixes[message.guild.id] = {
+         prefixes: config.prefix
+     };
+ }
+ let prefix = prefixes[message.guild.id].prefixes;
  let command = message.content.split(" ")[0];
  command = command.slice(prefix.length);
 
