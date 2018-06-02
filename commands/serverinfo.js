4bd05fb1a1237f7813235db5e89f798930d8814c
WilsonGaara <37121741+WilsonGaara@users.noreply.github.com>
2018-06-02 13:30:34 -0300

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -104,7 +104,7 @@ exports.run = (client, message, args)  => {
              .addField(`🌟 Entrei aqui em`,moment(message.guild.joinedAt).format(`lll`), true)
              .addField(`👥 Membros (${message.guild.memberCount})`, `🙋 **Pessoas:** ${message.guild.memberCount - message.guild.members.filter(member => member.user.bot).size}\n🤖 **Robôs:** ${message.guild.members.filter(member => member.user.bot).size}`, true)
              .setColor('BLUE')
-             .setThumbnail(message.guild.icon)
+             .setThumbnail(message.guild.iconURL)
              .setTimestamp()
              .setFooter(message.guild)
                    message.channel.send({embed: embed2});
