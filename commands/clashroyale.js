3215d7b7a55fcfd73392263891042931cff06a95
WilsonGaara <37121741+WilsonGaara@users.noreply.github.com>
2018-06-03 17:41:57 -0300

    Update clashroyale.js

--- commands/clashroyale.js
+++ commands/clashroyale.js
@@ -2,7 +2,7 @@ exports.run = (bot, message, args)  => {
   const CRoyale = require('croyale');
 const client = new CRoyale.Client(process.env.CR);
  
-client.getPlayer('CVLQ2GV8', { keys: ['name'] })
+client.getPlayer('CVLQ2GV8')
     .then(player => {
         console.log(`The player's name is ${player.name}`);
   console.log(player)
