fc07366a3f7559de357011b594f528e2b42e1301
WilsonGaara <37121741+WilsonGaara@users.noreply.github.com>
2018-06-05 12:57:58 -0300

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,17 +1,13 @@
 exports.run = (client, message, args)  => {
+    
 	const Discord = require("discord.js");
-	  if (message.mentions.users.first()) {
-    user = message.mentions.users.first().username;
-    userfoto = message.mentions.users.first().displayAvatarURL;
-  } else {
-    user = message.author.username
-    userfoto = message.author.avatarURL;
-  }
 
+  let user = message.mentions.users.first().username || message.author.username || message.guild.members.get(args.join(' ').username)
+  let user2 = message.mentions.users.first().displayAvatarURL || message.author.displayAvatarURL || message.guild.members.get(args.join(' ').displayAvatarURL)
         const embed = new Discord.RichEmbed()
              .setColor('AQUA')
              .setTitle(`:frame_photo: ${user}`)
-             .setDescription(`**Clique [aqui](${userfoto}) para abrir o link do seu avatar, ou o link de download**`)
+             .setDescription(`**Clique [aqui](${user2}) para abrir o link do seu avatar, ou o link de download**`)
              .setImage(userfoto);
   
         message.channel.send({embed: embed})
