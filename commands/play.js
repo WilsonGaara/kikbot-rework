6b7267cf1c0ae13801cf4705a47703b97b7ad84f
WilsonGaara <37121741+WilsonGaara@users.noreply.github.com>
2018-06-09 13:20:52 -0300

    Create play.js

---
+++ commands/play.js
@@ -0,0 +1,19 @@
+exports.run = async (client, message, args) => {
+var video;
+const ytdl = require("ytdl-core");
+ytSearch = require('yt-search');
+ ytSearch(args.join(" "), async function (err, r) {
+   if ( err ) throw err
+   const videos = r.videos
+   video = videos[0]
+   console.log(video)
+   let musica = "www.youtube.com" + video.url
+   let voiceChannel = message.member.voiceChannel
+   voiceChannel.join()
+   .then(conneciton => {
+     const stream = ytdl(musica, { filter : 'audioonly' });
+     const dispatcher = connection.playStream(stream)
+     dispatcher.setVolumeLogarithmic(5 / 5);
+   });
+  });
+};
