1360bfbde0da7e7ff12e90432bdb3ed763c27d17
WilsonGaara <37121741+WilsonGaara@users.noreply.github.com>
2018-06-09 17:26:20 -0300

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -42,37 +42,39 @@ exports.run = (client, message, args)  => {
   
       }
     }
-    function regiao() {
-      var guildVerification = message.guild.region
-      if  (guildVerification == "en-us") {
-      return ":flag_us: Estados Unidos"
-    } else if (guildVerification == `brazil`) {
-      return (":flag_br: Brasil")
-          } else if  (guildVerification == "russia") {
-      return (":flag_ru: Rússia")
-      } else if (guildVerification == "singapore") {
-      return (":flag_sg: Singapura")
-          } else if (guildVerification == "hongkong") {
-      return (":flag_hk: Hong Kong")
-    } else if (guildVerification == "unnamed") {
-      return ("<:blobfacepalm:369904810784325635> l Eu não fiz mais functions, desculpe, volte mais tarde")
-    } else if (guildVerification == "eu-west") {
-      return (":flag_eu: Europa Oeste")
-    } else if (guildVerification == "us-east") {
-      return (":flag_us: Costa Leste dos Estados Unidos")
-    } else if (guildVerification == "sydney") {
-      return (":flag_au: Sydney (Dentro da austrália)")
-    } else if (guildVerification == "us-central") {
-      return (":flag_us: Estados Unidos Central")
-    } else if (guildVerification == "us-south") {
-      return (":flag_us: Região Sul dos Estados Unidos")
-    } else if (guildVerification == "us-west") {
-      return (":flag_us: Região Oeste dos Estados Unidos")
-    } else if (guildVerification == "london") {
-      return (":flag_gb: Londres ")
-      
-      }
-    }
+   function regiao() {
+	var guildVerification = message.guild.region
+	if  (guildVerification == "en-us") {
+	return ":flag_us: Estados Unidos"
+  } else if (guildVerification == `brazil`) {
+	return (":flag_br: Brasil")
+		} else if  (guildVerification == "russia") {
+	return (":flag_ru: Rússia")
+	} else if (guildVerification == "singapore") {
+	return (":flag_sg: Singapura")
+		} else if (guildVerification == "hongkong") {
+	return (":flag_hk: Hong Kong")
+  } else if (guildVerification == undefined) {
+	return (":face_palm: **|** Eu não fiz mais functions, desculpe, volte mais tarde")
+  } else if (guildVerification == "eu-west") {
+	return (":flag_eu: Europa Oeste")
+  } else if (guildVerification == "us-east") {
+	return (":flag_us: Costa Leste dos Estados Unidos")
+  } else if (guildVerification == "sydney") {
+	return (":flag_au: Sydney (Dentro da austrália)")
+  } else if (guildVerification == "us-central") {
+	return (":flag_us: Estados Unidos Central")
+  } else if (guildVerification == "us-south") {
+	return (":flag_us: Região Sul dos Estados Unidos")
+  } else if (guildVerification == "us-west") {
+	return (":flag_us: Região Oeste dos Estados Unidos")
+  } else if (guildVerification == "london") {
+	return (":flag_gb: Londres ")
+  } else if(guildVerification == "japan") {
+	  return (":flag_jp: Japão")
+	
+	}
+  }
   
       
   
