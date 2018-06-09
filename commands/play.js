c6b08485e3b7a817496716b0838f850700274303
WilsonGaara <37121741+WilsonGaara@users.noreply.github.com>
2018-06-09 16:58:16 -0300

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -1,26 +1,26 @@
-exports.run = async (client, message, args) => {
-var video;
-const ytdl = require("ytdl-core");
-ytSearch = require('yt-search');
- ytSearch(args.join(" "), async function (err, r) {
-   if ( err ) throw err
-   const videos = r.videos
-   video = videos[0]
-   console.log(video)
-let musica = "www.youtube.com" + video.url
+//exports.run = async (client, message, args) => {
+//var video;
+//const ytdl = require("ytdl-core");
+//ytSearch = require('yt-search');
+// ytSearch(args.join(" "), async function (err, r) {
+ //  if ( err ) throw err
+ //  const videos = r.videos
+//video = videos[0]
+ //  console.log(video)
+//let musica = "www.youtube.com" + video.url
 
-   let voiceChannel = message.member.voiceChannel
-  voiceChannel.join()
-   message.reply('🎧 **|** Aguardando tocar: `'+videos[ 0 ].title+'` **'+videos[ 0 ].duration.timestamp+'**')
-  .then(connection => {
-    const stream = ytdl(musica, { filter : 'audioonly' });
-    const dispatcher = connection.playStream(stream)
+ //  let voiceChannel = message.member.voiceChannel
+  //voiceChannel.join()
+  // message.reply('🎧 **|** Aguardando tocar: `'+videos[ 0 ].title+'` **'+videos[ 0 ].duration.timestamp+'**')
+  //.then(connection => {
+  //  const stream = ytdl(musica, { filter : 'audioonly' });
+  //  const dispatcher = connection.playStream(stream)
    
-     .on("end", () => {
-       message.channel.send('Música acabou!')
-       message.member.voiceChannel.leave();
-     });
-     dispatcher.setVolumeLogarithmic(5 / 5);
-   });
- });
-};
+  //   .on("end", () => {
+//       message.channel.send('Música acabou!')
+//message.member.voiceChannel.leave();
+//     });
+//     dispatcher.setVolumeLogarithmic(5 / 5);
+//   });
+// });
+//};
