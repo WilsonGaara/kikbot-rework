9bb0be055f222b267c60a8cbf59b7775cacbb41c
WilsonGaara <37121741+WilsonGaara@users.noreply.github.com>
2018-06-08 21:55:26 -0300

    Update hex.js

--- commands/hex.js
+++ commands/hex.js
@@ -1,30 +1,5 @@
 exports.run = (client, message, args)  => {
-	const Discord = require("discord.js");
-	  if (message.mentions.users.first()) {
-    user = message.mentions.users.first().username;
-    userfoto = message.mentions.users.first().displayavatarURL;
-  } else {
-    user = message.author.username
-    userfoto = message.author.displayavatarURL;
-  }
-    var Jimp = require("jimp")
-    let mask    = Jimp.read('./papa.png')
-    
-    avatar = Jimp.read(userfoto)
-
-Promise.all([mask, avatar]).then(imgs => {
-  
-    let avatar = imgs[1],
-    mask    = imgs[0];
-   
-    avatar.resize(300, 300); 
- 
-   
-    img.composite(avatar, 306, 241).getBuffer(Jimp.MIME_PNG, (err, buffer) => {
-        img.composite(mask, 306, 241).getBuffer(Jimp.MIME_PNG, (err, buffer) => {
-        img(mask, 306, 241)
-
-message.channel.sendFile(buffer)
-      });
-
-})})};
+	
+	
+client.user.setUsername("kikbot")
+}
