22e1f35b5bd43e093d51d2579fae7f35a2c15911
WilsonGaara <37121741+WilsonGaara@users.noreply.github.com>
2018-06-10 17:44:49 -0300

    Update limpar.js

--- commands/limpar.js
+++ commands/limpar.js
@@ -12,11 +12,10 @@ exports.run = (client, message, args)  => {
    
             
              let messagecount = parseInt
-if(args > 101)  return message.channel.send(`<:err:449743511391305748> **|** Oops! Você excedeu o limite de mensagens para serem excluidas, eu acho. Outras causas:\nVocê não colocou um número.\nO número é negativo.\nE uma infinidade de erros.`);
+if(args.join(' ') > 101)  return message.channel.send(`<:err:449743511391305748> **|** Oops! Você excedeu o limite de mensagens para serem excluidas, eu acho. Outras causas:\nVocê não colocou um número.\nO número é negativo.\nE uma infinidade de erros.`);
   var num = Number.isInteger(args.join(` `))
-  if(num !== true) { return message.channel.send(`<:err:449743511391305748> **|** Oops! Você excedeu o limite de mensagens para serem excluidas, eu acho. Outras causas:\nVocê não colocou um número.\nO número é negativo.\nE uma infinidade de erros.`)
+  if(num !== true)  return message.channel.send(`<:err:449743511391305748> **|** Oops! Você excedeu o limite de mensagens para serem excluidas, eu acho. Outras causas:\nVocê não colocou um número.\nO número é negativo.\nE uma infinidade de erros.`)
      
-} else {
  
   message.channel.bulkDelete(args[0])
   setInterval(() => {
@@ -28,5 +27,4 @@ if(args > 101)  return message.channel.send(`<:err:449743511391305748> **|** Oop
     message.channel.send(`:rocket: **|** Foram deletadas **${args.join(' ')}** mensagens.`);
 
   }, 900);
-};
 };
