a1beca4c9701ca3779f5ae36e1ec7311b98c874c
WilsonGaara <37121741+WilsonGaara@users.noreply.github.com>
2018-06-10 17:31:41 -0300

    Update limpar.js

--- commands/limpar.js
+++ commands/limpar.js
@@ -1,23 +1,22 @@
-    exports.run = (client, message, args)  => {
+exports.run = (client, message, args)  => {
 
-        const Discord2 = require('discord.js');
-       
-           const embed2 = new Discord2.RichEmbed()
-       .setAuthor(message.author.tag, message.author.displayAvatarURL)
-       .setColor('BLUE')
-       .setDescription(':thinking: | k!limpar\nApague algumas mensagens de um canal de texto!\n\n:information_desk_person: **Como usar:** k!limpar <quantia.de.mensagens>\n\n:book: **Exemplo:**\nk!limpar `50`')
-           .setFooter(`Utilitários • Website: kikbot-.glitch.me`, message.author.displayAvatarURL)
-           if(args.length < 1) return message.channel.send({embed : embed2})
-         
-         
-                   if  (!message.member.hasPermissions(["MANAGE_MESSAGES"])) return message.reply("**Você não tem a permissão para usar isto!**");
-                   let messagecount = parseInt
-       message.channel.bulkDelete(args[0])
-     if(!args.join(' ') > 100) { return message.channel.send(`<:err:449743511391305748> **|** Oops! Você excedeu o limite de mensagens para serem excluidas, eu acho. Outras causas:\nVocê não colocou um número.\nO número é negativo.\nE uma infinidade de erros.`);
-        if(!args.join(' ') < 2) { message.channel.send(`<:err:449743511391305748> **|** Oops! Você excedeu o limite de mensagens para serem excluidas, eu acho. Outras causas:\nVocê não colocou um número.\nO número é negativo.\nE uma infinidade de erros.`);
-      } else {
-            message.channel.send(`:rocket: **|** Foram deletadas **${args.join(' ')}** mensagens.`);
-      };
-    };
+  const Discord2 = require('discord.js');
+ 
+     const embed2 = new Discord2.RichEmbed()
+ .setAuthor(message.author.tag, message.author.displayAvatarURL)
+ .setColor('BLUE')
+ .setDescription(':thinking: | k!limpar\nApague algumas mensagens de um canal de texto!\n\n:information_desk_person: **Como usar:** k!limpar <quantia.de.mensagens>\n\n:book: **Exemplo:**\nk!limpar `50`')
+     .setFooter(`Utilitários • Website: kikbot-.glitch.me`, message.author.displayAvatarURL)
+     if(args.length < 1) return message.channel.send({embed : embed2})
+   
+   
+             if  (!message.member.hasPermissions(["MANAGE_MESSAGES"])) return message.reply("**Você não tem a permissão para usar isto!**");
+             let messagecount = parseInt
+ message.channel.bulkDelete(args[0])
+if(args.join(' ') > 100)  return message.channel.send(`<:err:449743511391305748> **|** Oops! Você excedeu o limite de mensagens para serem excluidas, eu acho. Outras causas:\nVocê não colocou um número.\nO número é negativo.\nE uma infinidade de erros.`);
+  if(args.join(' ') < 2)  return message.channel.send(`<:err:449743511391305748> **|** Oops! Você excedeu o limite de mensagens para serem excluidas, eu acho. Outras causas:\nVocê não colocou um número.\nO número é negativo.\nE uma infinidade de erros.`);
+  var num = Number.isInteger(args.join(` `))
+  if(num === false) return message.channel.send(`<:err:449743511391305748> **|** Oops! Você excedeu o limite de mensagens para serem excluidas, eu acho. Outras causas:\nVocê não colocou um número.\nO número é negativo.\nE uma infinidade de erros.`)
+      message.channel.send(`:rocket: **|** Foram deletadas **${args.join(' ')}** mensagens.`);
 };
-       
+
