78807658318cf5d04530ca262cd047a7772910d8
WilsonGaara <37121741+WilsonGaara@users.noreply.github.com>
2018-06-12 15:44:41 -0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,11 @@
+
 exports.run = (client, message, args)  => {
 	const Discord = require('discord.js');
 
 		message.react(`🏓`);
 
 	
-	message.reply('📡 **l** `'+Math.round(client.ping)+'ms`' + ' Shard **|** '+ client.shard.id + 1 +'/2\n`❔` **|** Este comando te fala sobre minha taxa de ida e volta, em milisegundos, isto é. Meu **ping**. ');
+	message.reply('📡 **l** `'+Math.round(client.ping)+'ms`' + ' Shard **|** '+`${(client.shard.id +1)}`+'/2\n`❔` **|** Este comando te fala sobre minha taxa de ida e volta, em milisegundos, isto é. Meu **ping**. ');
 	 
 
 
