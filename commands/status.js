133cead1936b7273d87dd32d8be51730f0061486
WilsonGaara <37121741+WilsonGaara@users.noreply.github.com>
2018-06-12 12:51:15 -0300

    Update status.js

--- commands/status.js
+++ commands/status.js
@@ -1,15 +1,16 @@
 exports.run = (client, message, args)  => {
-const Discord = require('discord.js')
-
-var embed2 = new Discord.RichEmbed()
-.setAuthor('Status | Kikbot', client.user.avatarURL)
-.setDescription('_Este comando fala para o autor meus status atuais(ou não)_')
-.addField('<:memory:437019126175891466> RAM', (` ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}mb`))
-.addField('🎛 CPU', (process.cpuUsage().user / 1024 / 1024 / 10).toFixed(2) + "%")
-.setColor('RED')
-.setTimestamp()
-.setFooter(message.author.tag, message.author.displayAvatarURL)
-      message.channel.send({embed: embed2});
-
-
-};
+    const Discord = require('discord.js')
+    
+    var embed2 = new Discord.RichEmbed()
+    .setAuthor('Status | Kikbot', client.user.avatarURL)
+    
+    .addField('<:memory:437019126175891466> RAM', (` ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}mb`), true)
+    .addField('🎛 CPU', (process.cpuUsage().user / 1024 / 1024 / 2).toFixed(2) + "%", true)
+    .addField('📚 Outras infos...', `**Usuários:** ${client.users.size}\n**Latência:** ${Math.round(client.ping)}\n**Servidores:** ${client.servers.size}`)
+    .setColor('RED')
+    .setTimestamp()
+    .setFooter(message.author.tag, message.author.displayAvatarURL)
+          message.channel.send({embed: embed2});
+    
+    
+    };
