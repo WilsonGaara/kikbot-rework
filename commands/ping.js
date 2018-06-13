c4221fa2b1a7e5216845afdc380df40c472a2981
WilsonGaara <37121741+WilsonGaara@users.noreply.github.com>
2018-06-13 20:13:18 -0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,12 +1,6 @@
-
 exports.run = (client, message, args)  => {
 	const Discord = require('discord.js');
 
 		message.react(`🏓`);
-
-	
-	message.reply('📡 **l** `'+Math.round(client.ping)+'ms`' + ' Shard **|** '+`${(client.shard.id +1)}`+'/2\n`❔` **|** Este comando te fala sobre minha taxa de ida e volta, em milisegundos, isto é. Meu **ping**. ');
-	 
-
-
-};
+message.channel.send(message.author + ' 📡 **|** `'+client.ping+'` (Shard: '+`${client.shard.id + 1}`+'/2)')
+}
