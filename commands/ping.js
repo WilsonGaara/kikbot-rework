48dbb5a3c010c173c2850153ee0b9b5b3e2c54d6
WilsonGaara <37121741+WilsonGaara@users.noreply.github.com>
2018-06-13 20:15:47 -0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,5 +2,5 @@ exports.run = (client, message, args)  => {
 	const Discord = require('discord.js');
 
 		message.react(`🏓`);
-message.channel.send(message.author + ' 📡 **|** `'+client.ping+'` (Shard: '+`${client.shard.id + 1}`+'/2)')
+message.channel.send(message.author + ' 📡 **|** `'+client.ping+'ms` (Shard: '+`**${client.shard.id + 1}**`+'/2)')
 }
