e2cbc2e0818b3621410cb035c9bc00cfec15b6f9
WilsonGaara <37121741+WilsonGaara@users.noreply.github.com>
2018-06-14 15:14:09 -0300

    Update captcha.js

--- commands/captcha.js
+++ commands/captcha.js
@@ -4,8 +4,14 @@ exports.run = (client, message, args)  => {
     const random = texto[Math.floor(Math.random() * texto.length)];
 	let CaptchaRole = message.guild.roles.find("name", `Membros`);
   if(!CaptchaRole) { return message.reply(":x: Não vi nenhum cargo com o nome de `Membros`");
+  
 } else {
 };
+if(message.member.roles.has(CaptchaRole.id)) {
+  message.guild.member(message.author.id).removeRole(CaptchaRole.id)
+message.author.send(`Cargo removido :frowning:`)
+
+} else {
 message.channel.send(random)
 if(args.join(" ").toLowerCase() == random) {
 		 message.guild.member(message.author.id).addRole(CaptchaRole.id).catch(console.error);
@@ -13,3 +19,4 @@ if(args.join(" ").toLowerCase() == random) {
 if(args.join(" ").toLowerCase() !== random) return message.channel.send('<:err:449743511391305748> **|** Você não acertou o captcha, tente novamente.')
     };
   };
+};
