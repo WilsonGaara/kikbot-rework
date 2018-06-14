1f0e68503d0e213fa6ce8955a8e49c743a013cb2
WilsonGaara <37121741+WilsonGaara@users.noreply.github.com>
2018-06-14 15:29:00 -0300

    Update captcha.js

--- commands/captcha.js
+++ commands/captcha.js
@@ -12,11 +12,13 @@ if(message.member.roles.has(CaptchaRole.id)) {
 message.author.send(`Cargo removido :frowning:`)
 
 } else {
-message.channel.send(random)
-if(args.join(" ").toLowerCase() == random) {
+message.channel.send(':white_check_mark: **|** Ok, agora o captcha é: **'+random+'**')
+const check = await message.channel.awaitMessages(m => m.author.id === message.author.id, { time: 5000 })
+if(check.toLowerCase() == random) {
 		 message.guild.member(message.author.id).addRole(CaptchaRole.id).catch(console.error);
      message.author.send(`Cargo adcionado em: ${message.guild}`)
-if(args.join(" ").toLowerCase() !== random) return message.channel.send('<:err:449743511391305748> **|** Você não acertou o captcha, tente novamente.')
+   message.channel.send(':white_check_mark: **|** Mano do céu, conseguiu brother.')
+}
+if(check.toLowerCase() !== random) return message.channel.send('<:err:449743511391305748> **|** Você não acertou o captcha, tente novamente.')
     };
   };
-};
