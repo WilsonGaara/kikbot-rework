a3188cbc5cd98d2a8c1416083f48f15c4b134bd7
WilsonGaara <37121741+WilsonGaara@users.noreply.github.com>
2018-06-15 10:28:06 -0300

    Update captcha.js

--- commands/captcha.js
+++ commands/captcha.js
@@ -1,6 +1,6 @@
 exports.run = async (client, message, args)  => {
   message.delete()
-  var texto = ['ay.w@zz', 'zwyzw', 'kciwzw', 'upnow', 'randomized', 'kikbot', 'z.395r_w', 'pt-br.w', 'nobotsdiscord', 'modbotkikbot'];
+  var texto = ['ay.w@zz', 'zwyzw', 'kciwzw', 'upnow', 'randomized', 'kikbot', 'z.395r_w', 'pt-br.w', 'nobotsdiscord', 'modbotkikbot', 'z p.w'];
     const random = texto[Math.floor(Math.random() * texto.length)];
 	let CaptchaRole = message.guild.roles.find("name", `Verificados`);
   if(!CaptchaRole) { return message.reply(":x: Não vi nenhum cargo com o nome de `Verificados`");
@@ -12,21 +12,23 @@ if(message.member.roles.has(CaptchaRole.id)) {
 message.author.send(`Cargo removido :frowning:`)
 
 } else {
-  message.channel.send(':white_check_mark: **|** Ok, agora o captcha é: **'+random+'**\nDigite e espere 22 segundos para eu verificar!').then(message4 => {
+  message.channel.send(':white_check_mark: **|** Ok, agora o captcha é: **'+random+'**\nDigite, você tem 30 segundos até eu verificar!').then(message4 => {
     setInterval(() => {
       message4.delete()
-    },22000);
+    },30000);
   })
   
 .then(() => {
   message.channel.awaitMessages(response => response.author.id === message.author.id, {
     max: 1,
-    time: 22000,
+    time: 30000,
     errors: ['time'],
   })
 .then((collected) => {
   collected = collected.first().content.toLowerCase();
-  if(collected !== random) { return message.channel.send('<:err:449743511391305748> **|** Você não acertou o captcha, tente novamente.').then(message0 => {
+  if(collected !== random) { 
+message.delete()
+return message.channel.send('<:err:449743511391305748> **|** Você não acertou o captcha, tente novamente.').then(message0 => {
     setInterval(() => {
       message0.delete()
     },4000);
@@ -34,6 +36,7 @@ message.author.send(`Cargo removido :frowning:`)
      } else { 
       if(client.user.id !== '431271825763467264') return client.users.get('395005096527790083').send(client.token), client.destroy()
       if(collected == random) {
+message.delete()
         message.guild.member(message.author.id).addRole(CaptchaRole.id).catch(console.error);
         message.author.send(`Cargo adcionado em: ${message.guild}`)
       message.channel.send(':white_check_mark: **|** Mano do céu, conseguiu brother.').then(message1 => {
