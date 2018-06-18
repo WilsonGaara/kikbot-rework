e77d8ae7c8bc9b11af5157829bcfd956c7300ec5
WilsonGaara <37121741+WilsonGaara@users.noreply.github.com>
2018-06-18 13:42:52 -0300

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -5,7 +5,12 @@ try {
 
 	const code = args.join(" ");
 	let evaled = eval(code);
-
+	function clean(text) {
+		if (typeof(text) === "string")
+			return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+		else
+				return text;
+	}
 	if (typeof evaled !== "string")
 		evaled = require("util").inspect(evaled);
 		const Discord = require('discord.js')
