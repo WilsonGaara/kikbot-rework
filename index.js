3b308de4459e07c52b672556ce5243b2bbfec5ff
WilsonGaara <37121741+WilsonGaara@users.noreply.github.com>
2018-06-18 13:19:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -67,16 +67,16 @@ client.on("message", message => {
   try {
     let commandFile = require(`./commands/${command}.js`);
     commandFile.run(client, message, args);
-    var texto = ['1', '2', '3', '4', '5', '6', '7', '8', '111', '20', '30']
+    var texto = ['1', '2', '3', '4', '5', '6', '7', '8']
  const random = texto[Math.floor(Math.random() * texto.length)];
-if(random == '3') return 
-var embed1 = new Discord.RichEmbed()
+ const Discord2 = require('discord.js');
+var embed1 = new Discord2.RichEmbed()
 .setTitle('😃 | Kikbot')
 .setDescription('<:kikbotcuted:458296375793418240> **|** Vejo que está gostando de meus comandos, se ainda não divulgou o **kikbot** para seus amigos. Peço que por favor, me divulgue, isso ajuda bastante.\n[Clique aqui para me adcionar em algum servidor.](https://discordapp.com/api/oauth2/authorize?client_id=431271825763467264&permissions=36826310&scope=bot)')
 .setTimestamp()
 .setColor('BLUE')
-.setFooter(client.user.username, client.user.avatarURL)
-if(random == '3') return message.channel.send(embed1)
+.setFooter(message.author.tag, client.user.avatarURL)
+if(random == '3') return message.channel.send({embed : embed1})
   } catch (err) {
     console.error(err);
   }
