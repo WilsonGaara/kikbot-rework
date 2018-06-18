9751120a37a9cdf3614604685af83e2f1ae9b742
WilsonGaara <37121741+WilsonGaara@users.noreply.github.com>
2018-06-18 10:57:33 -0300

    Update status.js

--- commands/status.js
+++ commands/status.js
@@ -4,17 +4,16 @@ exports.run = (client, message, args)  => {
 var seg = Math.floor(uptime/1000)%60;
 var min = Math.floor(uptime/(1000*60))%60;
 var horas = Math.floor(uptime/(1000*60*60))%24;
-var uptimeconv = `(${horas}) Hora(s) e (${min}) minutos.`
+var uptimeconv = `${horas}h, ${min}m e ${seg}s.`
 
 	var embed = new Discord.RichEmbed()
   .setAuthor('Status | Kikbot', client.user.avatarURL)
-  .setDescription('Mostrando informações sobre Fragmento(shard) ['+`${client.shard.id + 1}`+'] ')
+  .setDescription('💡 **|** Você sabia que estou à: '+uptimeconv+' on-line?')
 	 .setThumbnail(client.user.avatarURL)
-	.addField('<:memory:437019126175891466> RAM', (` ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}mb`), true)
-	.addField('🎛 CPU', (process.cpuUsage().user / 1024 / 1024 / 16).toFixed(2) + "%", true)
-	.addField('📚 Outras infos...', `**Usuários:** ${client.users.size}\n**Latência:** ${Math.round(client.ping)}ms\n**Servidores:** ${client.guilds.size}\n**Shard:** **${client.shard.id + 1}**/2`, true)
-	.addField(`🕙 Uptime`, uptimeconv, true)
-	.setColor('BLUE')
+        .addField('Status', `**RAM:** ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}mb\n**CPU:** ${(process.cpuUsage().user / 1024 / 1024 / 24).toFixed(2)}%`)
+  .addField('📚 Outras infos...', `**Usuários:** ${client.users.size}\n**Latência:** ${Math.round(client.ping)}ms\n**Servidores:** ${client.guilds.size}\n**Shard:** **${client.shard.id + 1}**/2`, true)
+  .addField('💻 Meu criador:', client.users.get('395005096527790083').tag)
+	.setColor('GREEN')
 	.setTimestamp()
 	.setFooter(message.author.tag, message.author.displayAvatarURL)
 				message.channel.send({embed: embed})
