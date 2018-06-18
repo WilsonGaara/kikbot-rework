381e64d472a3a919d852fd9daec1dbe2982713d1
WilsonGaara <37121741+WilsonGaara@users.noreply.github.com>
2018-06-18 11:31:07 -0300

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -13,7 +13,7 @@ var uptimeconv = `**${horas}**h, **${min}**m e **${seg}**s.`
         .addField('Status', `**RAM:** ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}mb\n**CPU:** ${(process.cpuUsage().user / 1024 / 1024 / 24).toFixed(2)}%`, true)
   .addField('📚 Outras infos...', `**Usuários:** ${client.users.size}\n**Latência:** ${Math.round(client.ping)}ms\n**Servidores:** ${client.guilds.size}\n**Shard:** **${client.shard.id + 1}**/2`, true)
   .addField('💻 Meu criador:', client.users.get('395005096527790083').tag, true)
-  .addField('📘 Links', '**-** [**WEBSITE**](http://www.kikbot-.glitch.me)\n**-** [**SUPORT GUILD**](https://discord.gg/sFQFK2Q)\n**-** [**OAUTH2**](https://discordapp.com/api/oauth2/authorize?client_id=431271825763467264&permissions=36826310&scope=bot)', true)
+  .addField('📘 Links', '**-** [**WEBSITE**](http://www.kikbot-.glitch.me)\n**-** [**SUPORT GUILD**](https://discord.gg/sFQFK2Q)\n**-** [**INVITE**](https://discordapp.com/api/oauth2/authorize?client_id=431271825763467264&permissions=36826310&scope=bot)', true)
 	.setColor('GREEN')
 	.setTimestamp()
 	.setFooter(message.author.tag, message.author.displayAvatarURL)
