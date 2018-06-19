fb1bae216cbfe4676964067d241e7fa26bcb3308
WilsonGaara <37121741+WilsonGaara@users.noreply.github.com>
2018-06-19 18:20:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -53,6 +53,7 @@ client.on("message", message => {
   if(!message.guild) return;
   if (message.author.bot) return;
   if (!message.content.startsWith(config.prefix)) return;
+  
  //Anti-Comando errado
 
  let command = message.content.split(" ")[0];
@@ -62,8 +63,9 @@ client.on("message", message => {
   let args = message.content.split(" ").slice(1);
   
 
-
-
+if(args.includes("<@431271825763467264>")) {
+message.channel.send(`👋 **|** Menção? Oh, certo digite **k!help** para saber mais sobre mim.`)
+}
   try {
     let commandFile = require(`./commands/${command}.js`);
     commandFile.run(client, message, args);
