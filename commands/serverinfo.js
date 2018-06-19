d9d697af7d8237eee4e845187e0af1be45804d64
WilsonGaara <37121741+WilsonGaara@users.noreply.github.com>
2018-06-19 19:38:51 -0300

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -174,7 +174,7 @@ function grandi2(){
 							var convert = fala.replace('jpg', 'png');
 						
 							 if(!client.guilds.get(args[0])) {
-							var embed2 = new Discord2.RichEmbed()
+							var embed2 = new Discord.RichEmbed()
 							.setAuthor(message.guild, convert)
 							.setDescription(grandi())
 							.addField('💻 ID' , message.guild.id, true)
@@ -193,7 +193,7 @@ function grandi2(){
 
 							let fala2 = client.guilds.get(args[0]).iconURL
 							var convert2 = fala2.replace('jpg', 'png')
-						var embed1 = new Discord2.RichEmbed()
+						var embed1 = new Discord.RichEmbed()
 						.setAuthor(client.guilds.get(args[0]).name, convert2)
 						.setDescription(grandi2())
 						.addField('💻 ID' , client.guilds.get(args[0]).id, true)
