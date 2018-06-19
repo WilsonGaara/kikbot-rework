37cfe826479a88187d98bb7f740d73a0b1bb86dd
WilsonGaara <37121741+WilsonGaara@users.noreply.github.com>
2018-06-19 11:44:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -67,6 +67,7 @@ client.on("message", message => {
   try {
     let commandFile = require(`./commands/${command}.js`);
     commandFile.run(client, message, args);
+  
     var texto = ['1', '2', '3', '4', '5', '6', '7', '8', '123', '11', '23']
  const random = texto[Math.floor(Math.random() * texto.length)];
  const Discord2 = require('discord.js');
