2b089ec2218baf21ef455912caf8940517b9e131
WilsonGaara <37121741+WilsonGaara@users.noreply.github.com>
2018-06-20 11:28:30 -0300

    Update report.js

--- commands/report.js
+++ commands/report.js
@@ -6,7 +6,7 @@ message.delete()
   if (message.mentions.users.first() < 0) return message.reply('Mencione alguém').catch(console.error);
   if (reason.length < 1) return message.reply('Coloque uma razão especifica! Ou. Mencione alguém!');
 if (message.mentions.users.first().id === message.author.id) return message.reply(`Se reportar?!`)
-  if (message.mentions.users.first().id === client.user.id) return message.reply(`O que que eu fiz ;(?!`)
+  if (message.mentions.users.first().id === client.user.id) return message.reply(`O que que eu fiz ;( `)
   const channel = message.guild.channels.find('name', 'kik-modlogs');
   // Do nothing if the channel wasn't found on this server
   if (!channel) return message.reply(`:x: **l** Erro! Não encontrei o canal #kik-modlogs`)
@@ -19,7 +19,7 @@ if (message.mentions.users.first().id === message.author.id) return message.repl
 user.send({embed});
   message.reply(`:white_check_mark: Prontinho!`,2500)
   channel.send(` :warning: **l** ${user} foi reportado por: ${message.author}  ("${reason}")`).then(message => {
-    message.react(`👍`)
-    message.react(`👎`)
+    message.react(`✅`)
+    message.react(`<:err:449743511391305748>`)
 
 })}
