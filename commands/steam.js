02794c64dfcb02d665fb9ced9e825571aaa60f71
WilsonGaara <37121741+WilsonGaara@users.noreply.github.com>
2018-06-25 20:02:49 -0300

    Update steam.js

--- commands/steam.js
+++ commands/steam.js
@@ -21,19 +21,29 @@ message.channel.send({embed: embed});
    //BLABLA
      
         const afk = games.map(g => g.name).join("\n")
- 
+ if(afk.length < 1) {
+
       const Discord2 = require('discord.js')
       const embed2 = new Discord2.RichEmbed()
    .setAuthor('Jogos recentes de: '+summary.nickname, games.iconURL)
-   .setDescription(`:white_check_mark: **|** Aqui está a lista de jogos recentes:\n${afk}`)
+   .setDescription(`:white_check_mark: **|** Aqui está a lista de jogos recentes:\n**Este usuário não possui jogos recentes...**`)
    .setImage(games.logoURL)
    .setTimestamp()
    .setFooter('kikbot- | Steam', client.user.avatarURL)
-  message.channel.send({embed: embed2});
+  return message.channel.send({embed: embed2});
 
   
-  
-});
-    });
+ }  else {
+   
+  const Discord23 = require('discord.js')
+  const embed2 = new Discord23.RichEmbed()
+.setAuthor('Jogos recentes de: '+summary.nickname, games.iconURL)
+.setDescription(`:white_check_mark: **|** Aqui está a lista de jogos recentes:\n${afk}}`)
+.setImage(games.logoURL)
+.setTimestamp()
+.setFooter('kikbot- | Steam', client.user.avatarURL)
+message.channel.send({embed: embed2});
+ }
+})
   });
-};
+    })}
