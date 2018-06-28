2807efc65f5789ada19bafc7bf1dd7408e50d7a4
WilsonGaara <37121741+WilsonGaara@users.noreply.github.com>
2018-06-27 22:52:48 -0300

    Update steamg.js

--- commands/steamg.js
+++ commands/steamg.js
@@ -2,7 +2,7 @@ exports.run = async (client, message, args) => {
     const SteamAPI = require('steamapi');
    const steam = new SteamAPI(process.env.STEAM);
    steam.getGameDetails(args[0]).then(summary => {
-  message.reply("<:err:449743511391305748> **|** Insira o ID de algum jogo steam. store.steampowered.com/app/**322330**\n322330 seria o steam do jogo Don't Starve Together.")
+if(!args[0]) return message.reply("<:err:449743511391305748> **|** Insira o ID de algum jogo steam. store.steampowered.com/app/**322330**\n322330 seria o steam do jogo Don't Starve Together.")
    const Discord = require('discord.js')
 let embed2 = new Discord.RichEmbed()
  .setAuthor(summary.name, 'https://webrazzi.com/wp-content/uploads/2013/01/steam.png')
