16dab37205e1c78061fed9a6b6ac22674405eb3d
WilsonGaara <37121741+WilsonGaara@users.noreply.github.com>
2018-06-28 09:54:23 -0300

    Update steamg.js

--- commands/steamg.js
+++ commands/steamg.js
@@ -1,11 +1,11 @@
-exports.run = async (client, message, args) => {
+
+try {
+  exports.run = async (client, message, args) => {
     const SteamAPI = require('steamapi');
    const steam = new SteamAPI(process.env.STEAM);
 if(args.join(" ").length < 1) return message.reply("<:err:449743511391305748> **|** Insira o ID de algum jogo steam. store.steampowered.com/app/**322330**\n322330 seria o steam do jogo Don't Starve Together.")
    steam.getGameDetails(args[0]).then(summary => {
-if(!steam) return message.reply("<:err:449743511391305748> **|** Insira o ID de algum jogo steam. store.steampowered.com/app/**322330**\n322330 seria o steam do jogo Don't Starve Together.")
-if(!summary) return message.reply("<:err:449743511391305748> **|** Insira o ID de algum jogo steam. store.steampowered.com/app/**322330**\n322330 seria o steam do jogo Don't Starve Together.")
-   const Discord = require('discord.js')
+ const Discord = require('discord.js')
 let embed2 = new Discord.RichEmbed()
  .setAuthor(summary.name, 'https://webrazzi.com/wp-content/uploads/2013/01/steam.png')
    .setDescription(summary.short_description)
@@ -20,3 +20,8 @@ let embed2 = new Discord.RichEmbed()
    message.channel.send({embed : embed, translate: true, lang: "pt"}).catch(error => message.reply(`Oops! Erro: ${error}`)) 
   
    })}
+
+} catch (e) {
+message.reply("<:err:449743511391305748> **|** Insira um ID de jogo na Steam válido por favor.")
+}
+  
