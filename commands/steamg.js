ea5a42ab1814b9d395b57060cca6e8304b2dc417
WilsonGaara <37121741+WilsonGaara@users.noreply.github.com>
2018-06-27 22:20:36 -0300

    Update steamg.js

--- commands/steamg.js
+++ commands/steamg.js
@@ -2,20 +2,19 @@ exports.run = async (client, message, args) => {
     const SteamAPI = require('steamapi');
    const steam = new SteamAPI(process.env.STEAM);
    steam.getGameDetails(args[0]).then(summary => {
-  
+  message.reply("<:err:449743511391305748> **|** Insira o ID de algum jogo steam. store.steampowered.com/app/**322330**\n322330 seria o steam do jogo Don't Starve Together.")
    const Discord = require('discord.js')
-   if(!summary.price_overview.discount_percent) {
-    let embed1 = new Discord.RichEmbed()
-    .setAuthor(summary.name, 'https://webrazzi.com/wp-content/uploads/2013/01/steam.png')
-    .setDescription(summary.short_description)
-    .setImage(summary.header_image)
-   return message.channel.send({embed : embed1, translate: true, lang: "pt"}).catch(error => message.reply("<:err:449743511391305748> **|** Insira o ID de algum jogo steam. store.steampowered.com/app/**322330**\n322330 seria o steam do jogo Don't Starve Together."))
-   } else {
+let embed2 = new Discord.RichEmbed()
+ .setAuthor(summary.name, 'https://webrazzi.com/wp-content/uploads/2013/01/steam.png')
+   .setDescription(summary.short_description)
+   
+   .setImage(summary.header_image)
+ if(summary.price_overview == undefined) return message.channel.send({embed : embed2, translate: true, lang: "pt"})
    let embed = new Discord.RichEmbed()
    .setAuthor(summary.name, 'https://webrazzi.com/wp-content/uploads/2013/01/steam.png')
    .setDescription(summary.short_description)
    .addField(':money_with_wings: Detalhes por preço', "Desconto de: "+summary.price_overview.discount_percent+"%")
    .setImage(summary.header_image)
-   message.channel.send({embed : embed, translate: true, lang: "pt"}).catch(error => message.reply("<:err:449743511391305748> **|** Insira o ID de algum jogo steam. store.steampowered.com/app/**322330**\n322330 seria o steam do jogo Don't Starve Together."))
+   message.channel.send({embed : embed, translate: true, lang: "pt"})
   
-   }})}
+   })}
