fb7d1cc9a38fe40eaac6e53e9cace77e11a768e7
WilsonGaara <37121741+WilsonGaara@users.noreply.github.com>
2018-06-27 22:58:00 -0300

    Update steamg.js

--- commands/steamg.js
+++ commands/steamg.js
@@ -2,7 +2,7 @@ exports.run = async (client, message, args) => {
     const SteamAPI = require('steamapi');
    const steam = new SteamAPI(process.env.STEAM);
    steam.getGameDetails(args[0]).then(summary => {
-if(!args[0]) return message.reply("<:err:449743511391305748> **|** Insira o ID de algum jogo steam. store.steampowered.com/app/**322330**\n322330 seria o steam do jogo Don't Starve Together.")
+if(args[0].length < 1) return message.reply("<:err:449743511391305748> **|** Insira o ID de algum jogo steam. store.steampowered.com/app/**322330**\n322330 seria o steam do jogo Don't Starve Together.")
    const Discord = require('discord.js')
 let embed2 = new Discord.RichEmbed()
  .setAuthor(summary.name, 'https://webrazzi.com/wp-content/uploads/2013/01/steam.png')
@@ -15,6 +15,6 @@ let embed2 = new Discord.RichEmbed()
    .setDescription(summary.short_description)
    .addField(':money_with_wings: Detalhes por preço', "Desconto de: "+summary.price_overview.discount_percent+"%")
    .setImage(summary.header_image)
-   message.channel.send({embed : embed, translate: true, lang: "pt"})
+   message.channel.send({embed : embed, translate: true, lang: "pt"}).catch(error => message.reply(`Oops! Erro: ${error}`)) 
   
    })}
