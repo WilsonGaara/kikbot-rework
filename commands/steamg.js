e4f5667e800320e380ebda6b4c6ef0f538b02445
WilsonGaara <37121741+WilsonGaara@users.noreply.github.com>
2018-06-27 21:49:16 -0300

    Update steamg.js

--- commands/steamg.js
+++ commands/steamg.js
@@ -8,15 +8,14 @@ exports.run = async (client, message, args) => {
     let embed1 = new Discord.RichEmbed()
     .setAuthor(summary.name, 'https://webrazzi.com/wp-content/uploads/2013/01/steam.png')
     .setDescription(summary.short_description)
-    .addField(':money_with_wings: Detalhes por preço', "Desconto de: "+summary.price_overview.discount_percent+"%")
     .setImage(summary.header_image)
-    message.channel.send({embed : embed1, translate: true, lang: "pt"})
+   return message.channel.send({embed : embed1, translate: true, lang: "pt"}).catch(error => message.reply("<:err:449743511391305748> **|** Insira o ID de algum jogo steam. store.steampowered.com/app/**322330**\n322330 seria o steam do jogo Don't Starve Together."))
    } else {
    let embed = new Discord.RichEmbed()
    .setAuthor(summary.name, 'https://webrazzi.com/wp-content/uploads/2013/01/steam.png')
    .setDescription(summary.short_description)
    .addField(':money_with_wings: Detalhes por preço', "Desconto de: "+summary.price_overview.discount_percent+"%")
    .setImage(summary.header_image)
-   message.channel.send({embed : embed, translate: true, lang: "pt"}).catch(message.reply("<:err:449743511391305748> **|** Insira o ID de algum jogo steam. store.steampowered.com/app/**322330**\n322330 seria o steam do jogo Don't Starve Together."))
+   message.channel.send({embed : embed, translate: true, lang: "pt"}).catch(error => message.reply("<:err:449743511391305748> **|** Insira o ID de algum jogo steam. store.steampowered.com/app/**322330**\n322330 seria o steam do jogo Don't Starve Together."))
   
    }})}
