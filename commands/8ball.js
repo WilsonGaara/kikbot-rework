07134f4dd0bbd47b4c2645ffe597dbbe1301c9b2
WilsonGaara <37121741+WilsonGaara@users.noreply.github.com>
2018-07-10 00:05:24 -0300

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -7,9 +7,9 @@ exports.run = (client, message, args)  => {
   } else {
   if(!argns.size < 1) return message.reply(`Espero que você esteja cansado(a) de tanto digitar... `)
 	message.react(`🎱`)
-	var texto = ['Eu tenho uma pequena certeza que sim.', 'Não, acho que não viu.', 'Como assim? Tente denovo', 'Alô?! TERRA?!', 'Hmm, acho que sim em!', 'Acho que não...']
+	var texto = ['Eu tenho uma pequena certeza que sim.', 'Não, acho que não viu.', 'Como assim? Tente denovo', 'Alô?! TERRA?!', 'Hmm, acho que sim em!', 'Acho que não...', 'Ahh! Pergunte pra outro', 'Sua vida é tão ruim assim?!']
     const random = texto[Math.floor(Math.random() * texto.length)];
-    message.channel.send(message.author+' 🎱 **|** Minha resposta é: '+random+'\n🤔 **|** A resposta muda a cada novo uso do comando (resposta aleatória)')
+    message.channel.send(message.author+' 🎱 **|** Minha resposta é: '+random+'\n🔮 **|** A resposta muda a cada novo uso do comando (resposta aleatória)')
   }
 
 }
