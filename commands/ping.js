12c26f5d3e0cf245b068f7dab7350967a5967d95
WilsonGaara <37121741+WilsonGaara@users.noreply.github.com>
2018-07-22 17:11:15 -0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,9 @@
 exports.run = (client, message, args)  => {
 	const Discord = require('discord.js');
 
-		message.react(`🏓`);
-message.channel.sendMessage(message.author + ' 📡 **|** `'+parseInt(client.ping)+'ms` (Shard: '+`**${client.shard.id + 1}**`+'/2)')
+message.channel.send(`:nerd: **|** Calculando latência, aguarde.`).then(msg1 => {
+
+
+msg1.edit('🏓 **|** Latência calculada da mensagem editada: '+`**${Date.now() - msg1.createdTimestamp}**`+'ms\n❤ Batimento cardiaco: (ping - websocket) `'+Math.round(client.ping)+'`ms (📡 **|** Shard: **'+`${client.shard.id + 1}**/2)\n🆘 **|** Caso esteja tendo problemas com conexão do Discord verifique sua rede ou acesse: https://status.discordapp.com`)
+})
 }
